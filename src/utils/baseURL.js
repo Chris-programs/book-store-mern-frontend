8150b2f1eaf8bdf9b7107b4b6da28cb5bac97126
Chris <yyhchris1222@gmail.com>
2026-08-24 22:48:27 +0800

    url updated

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,5 +1,8 @@
 const getBaseUrl = () => {
-  return 'http://localhost:5000';
+  return (
+    // 'http://localhost:5000',
+    'https://book-store-mern-backend-lyart.vercel.app'
+  );
 };
 
 export default getBaseUrl;
